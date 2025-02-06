4b25a4d2b264316d7bd93e20221f6a97d63ee88f
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-02-06 15:22:03 -0300

    Atualizado components: detalhes e notFound

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+import styles from './notFound.module.css'
 
 export function NotFound(){
     return(
-        <section>
-            
+        <section className={styles.container}>
+            <div className={styles.notFound}>
+                <h1>404 Page is not found</h1>
+                <Link to='/'>Página incial</Link>
+            </div>
         </section>
     )
 }
